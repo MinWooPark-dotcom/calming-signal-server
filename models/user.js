e0b0345e351b91ee95f96ed4b353733ed3c80304
Minwoo Park <positive1attraction@gmail.com>
2021-04-20 15:24:31 +0900

    [Add] a salt to the User column

--- models/user.js
+++ models/user.js
@@ -1,22 +1,33 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      models.User.hasMany(models.Post, { foreignKey: 'userId', sourceKey: 'id'});
-      models.User.hasMany(models.Comment, { foreignKey: 'userId', sourceKey: 'id'});
-      models.User.hasMany(models.Pet, { foreignKey: 'userId', sourceKey: 'id'});
+      models.User.hasMany(models.Post, {
+        foreignKey: "userId",
+        sourceKey: "id",
+      });
+      models.User.hasMany(models.Comment, {
+        foreignKey: "userId",
+        sourceKey: "id",
+      });
+      models.User.hasMany(models.Pet, {
+        foreignKey: "userId",
+        sourceKey: "id",
+      });
     }
-  };
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  }
+  User.init(
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      salt: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
