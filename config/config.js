0b94b2958bcb680936a41fe9dcbb631db49a1bec
Minwoo Park <positive1attraction@gmail.com>
2021-04-28 17:24:18 +0900

    [Fix] production

--- config/config.js
+++ config/config.js
@@ -18,10 +18,10 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME_PRODUCTION,
+    password: process.env.DATABASE_PASSWORD_PRODUCTION,
+    database: process.env.DATABASE_NAME_PRODUCTION,
+    host: process.env.DATABASE_HOST_PRODUCTION,
     dialect: "mysql",
   },
 };
